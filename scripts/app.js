5d4c12a86cbdc5233b1017e3b4a54833fbf53fe8
YGainey <yousef.gainey@gmail.com>
2024-06-17 10:08:09 +0100

    fixed changes: removed all but 1 audio element in HTML, play() only works on single instance. Adapated JS code to work for this and now it works)
    2

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,39 @@
 const buttons = document.querySelectorAll('button')
 console.dir(buttons)
-const audios = document.querySelectorAll('audio')
-console.dir(audios)
+const audio = document.querySelector('audio')
+console.dir(audio)
 
-const music =[
-    "./sounds/work_it.wav",
-    "./sounds/make_it.wav",
-    "./sounds/do_it.wav",
-    "./sounds/makes_us.wav",
-    "./sounds/harder.wav" ,
-    "./sounds/better.wav",
-    "./sounds/faster.wav",
-    "./sounds/stronger.wav",
-    "./sounds/more_than.wav",
-    "./sounds/hour.wav",
-    "./sounds/our.wav",
-    "./sounds/never.wav",
-    "./sounds/ever.wav",
-    "./sounds/after.wav",
-    "./sounds/work_is.wav",
-    "./sounds/over.wav"
-]
+// const music =[
+//     "./sounds/work_it.wav",
+//     "./sounds/make_it.wav",
+//     "./sounds/do_it.wav",
+//     "./sounds/makes_us.wav",
+//     "./sounds/harder.wav" ,
+//     "./sounds/better.wav",
+//     "./sounds/faster.wav",
+//     "./sounds/stronger.wav",
+//     "./sounds/more_than.wav",
+//     "./sounds/hour.wav",
+//     "./sounds/our.wav",
+//     "./sounds/never.wav",
+//     "./sounds/ever.wav",
+//     "./sounds/after.wav",
+//     "./sounds/work_is.wav",
+//     "./sounds/over.wav"
+// ]
 
-let ind = 0
-audios.forEach((evt) => {
-    evt.setAttribute('src', music[ind])
-    ind++
-})
+// let ind = 0
 
-const playAudio = (evt) =>{
-    // audios.playAudio()
-    audios.play()
+// audio.forEach((evt) => {
+//     evt.setAttribute('src', music[ind])
+//     ind++
+// })
+
+function playAudio(evt){
+    //console.log(evt.target.id)
+    //console.log(`./sounds/${evt.target.id}.wav`)
+    audio.src =`./sounds/${evt.target.id}.wav`
+    audio.play()
 }
 
 buttons.forEach((button) =>{
